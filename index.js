33ae64fb018c1309ff71b1e601a36d8df747f5db
Nuzhath Ideas <nuzhathideasfze@gmail.com>
2016-09-04 17:19:15 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>How to Book Desert Safari Online?</h1>
+
+<p id="desert">Desert Safari Booking Form</p>
+
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello Desert Safari!'">Click Me to Book Desert Safari!</button>
+
+</body>
+</html>
